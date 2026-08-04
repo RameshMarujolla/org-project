4d9a6fee87cdee052d34c71172215db12b5e3fc6
RameshMarujolla <ramesh.btp69@gmail.com>
2026-08-04 14:12:17 +0530

    chore: adjust formatting for consistency in main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_name           = var.vpc_name
-  cidr_block         = var.vpc_cidr
-  cluster_name       = var.cluster_name
-  public_subnets      = var.public_subnets
-  private_subnets    = var.private_subnets
+  vpc_name        = var.vpc_name
+  cidr_block      = var.vpc_cidr
+  cluster_name    = var.cluster_name
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
