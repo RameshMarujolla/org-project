b2c96a45d199635d3cbe57ea7b4e246a87a7e5a6
RameshMarujolla <ramesh.btp69@gmail.com>
2026-08-04 14:00:50 +0530

    fix: correct typo in public_subnets variable assignment in main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -4,6 +4,6 @@ module "vpc" {
   vpc_name           = var.vpc_name
   cidr_block         = var.vpc_cidr
   cluster_name       = var.cluster_name
-  ublic_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
+  public_subnets      = var.public_subnets
+  private_subnets    = var.private_subnets
 }
